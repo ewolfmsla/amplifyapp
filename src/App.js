8dc6a7b05961de7f1cc9cca4eb63105cbd9ae2f2
Eric Wolf <ewolf49@gmail.com>
2022-01-12 12:31:26 -0700

    changed main page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello From Missoula, MT!
         </p>
         <a
           className="App-link"
